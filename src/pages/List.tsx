30d0b4f84e232855e42ab8c5239bf85f5f9059bc
kkSpeedFire <k.kocot0@gmail.com>
2022-04-18 13:58:36 +0100

    done

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -2,39 +2,43 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useTodoContext } from "../components/TodoContext";
 export const List = () => {
-  const { items, addItem, deleteItem } = useTodoContext();
+  const { items, addItem } = useTodoContext();
   const [newItem, setNewItem] = useState("");
   const Todo = items.map((item) => (
     <li
-      className=" rounded-md flex bg-brown text-white items-center p-2 text-xl w-52 gap-10 "
+      className="h-fit drop-shadow-xl flex-col items-start rounded-lg flex bg-brown color-white  p-6 text-xl w-52 gap-6"
       key={item.id}
     >
-      <Link to={`/task/${item.id}`}>
-        <span>{item.value}</span>
+      <h2 className="wrapword text-xl font-semibold">{item.value}</h2>
+      <div className="wrapword text-sm">{item.details}</div>
+      <Link className=" self-end justify-self-end" to={`/task/${item.id}`}>
+        <button className=" bg-light-brown color-brown p-0.25 px-3 rounded font-semibold">
+          Edit
+        </button>
       </Link>
-      <button
-        className=" bg-light-brown color-brown p-2 rounded font-semibold"
-        onClick={() => deleteItem(item.id)}
-      >
-        Delete
-      </button>
     </li>
   ));
+  const handleKeypress = (e) => {
+    if (e.keyCode === 13) {
+      addItem(newItem, setNewItem);
+    }
+  };
 
   return (
-    <div className="flex flex-col justify-center align-center gap-24 w-full mt-36 m-14">
+    <div className="flex flex-col justify-center align-center gap-24 w-full mt-24 m-14">
       <h1 className="flex self-center text-3xl font-black color-brown">
         Todo App
       </h1>
       <div className="flex flex-wrap gap-5 justify-center">
         <input
-          className="text-xl w-80 bg-brown p-2 rounded text-white"
+          className="text-xl w-80 bg-brown p-2 rounded color-white"
           onChange={(e) => setNewItem(e.target.value)}
+          onKeyDown={handleKeypress}
           value={newItem}
           type="text"
         />
         <button
-          className="text-xl bg-brown text-white p-2 rounded"
+          className="text-xl bg-brown text-white  px-8 rounded font-semibold"
           onClick={() => addItem(newItem, setNewItem)}
         >
           Add
--- src/pages/Task.tsx
+++ src/pages/Task.tsx
@@ -12,19 +12,25 @@ export const Task = () => {
     editDetails(+id, e.target.value);
   };
   return (
-    <div className="flex flex-col gap-10 text-center justify-center text-xl w-1/2 m-7">
-      <h1 className="font-black text-2xl">{item.value}</h1>
-      <textarea onChange={edit} value={newDetails} className="  w-auto h-64" />
-      <div className="flex gap-8 justify-center m-7">
+    <div className="flex flex-col gap-24 text-center justify-center text-xl w-1/2 m-24">
+      <h1 className="wrapword  color-brown font-black text-3xl">
+        {item.value}
+      </h1>
+      <textarea
+        onChange={edit}
+        value={newDetails}
+        className=" p-4 color-white rounded-xl bg-brown w-auto h-64"
+      />
+      <div className=" flex-wrap flex gap-6 justify-between ">
         <Link to="/">
-          <button className=" bg-yellow-500 text-white p-2 rounded">
-            Back
+          <button className="px-6 bg-brown color-white p-1 rounded">
+            Save
           </button>
         </Link>
         <Link to="/">
           <button
             onClick={() => deleteItem(item.id)}
-            className="bg-red-600 text-white p-2 rounded"
+            className="bg-brown color-white px-6 p-1 rounded"
           >
             Delete
           </button>
